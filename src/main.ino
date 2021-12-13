f6a73ebb8d9413a37f902ff9879640cc4df6a5b3
Marco <marcovannoord@users.noreply.github.com>
2021-12-13 17:12:45 +0100

    processor draait nu langzamer, voor minder stroomverbruik

--- src/main.ino
+++ src/main.ino
@@ -1,13 +1,15 @@
 #include <Arduino.h>
-#define PIN_LED_PWM 11
+#include <avr/power.h>
+#define PIN_LED_PWM 10
 #define PIN_CURRENT_DRAIN 2             // deze pin verbinden via een 10k weerstand met de Drain van de mosfet
 #define PIN_MEASURE_VOLTAGE_DROP A1     // Deze pin via een 10k weerstand verbinden met de drain van de mosfet, en met een 10k weerstand verbinden met de GND
-#define MINIMUM_VOLTAGE_DROP_VALUE 170  // minimum waarde die gelezen moet worden over de sense-weerstand, voordat we vinden dat de ledstrip aan mag gaan
+#define MINIMUM_VOLTAGE_DROP_VALUE 15  // minimum waarde die gelezen moet worden over de sense-weerstand, voordat we vinden dat de ledstrip aan mag gaan
 
 bool led_staat_aan = false;
 
 void setup() {
     // put your setup code here, to run once:
+    clock_prescale_set(clock_div_4);
     pinMode(PIN_LED_PWM, OUTPUT);
     pinMode(PIN_CURRENT_DRAIN, OUTPUT);
     pinMode(PIN_MEASURE_VOLTAGE_DROP, INPUT);
@@ -32,9 +34,8 @@ bool meetAanwezigheidLedstrip() {
     if (led_staat_aan) {
         digitalWrite(PIN_LED_PWM, LOW);
     }
-
     digitalWrite(PIN_CURRENT_DRAIN, LOW);  // er gaat nu stroom lopen vanaf de ledstrip naar de arduino, als die aanwezig is. De ledstrip zal een klein beetje oplichten
-    // delay(1);
+    delayMicroseconds(10); // delay a very very short time, for the voltage to stabilize
     int input = analogRead(PIN_MEASURE_VOLTAGE_DROP);  // lees de spanning over de sense-weerstand
     digitalWrite(PIN_CURRENT_DRAIN, HIGH);             // stop de stroom naar de arduino
     if (led_staat_aan)                                 // als de led hiervoor aanstond, zet deze weer aan
@@ -53,7 +54,7 @@ bool meetAanwezigheidLedstrip() {
 void gaLangzaamAan() {
     for (size_t i = 0; i <= 255; i++) {
         analogWrite(PIN_LED_PWM, i);
-        delay(50);  // speel hiermee om in te stellen hoe snel hij aan moet gaan
+        delay(25);  // speel hiermee om in te stellen hoe snel hij aan moet gaan
     }
     Serial.println("Led staat nu volledig aan");
     led_staat_aan = true;  // onthoud dat de led aan staat, zodat we niet opnieuw gaan faden
